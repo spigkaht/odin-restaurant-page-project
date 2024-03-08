f2eef97b04e5bfdeb6c6f1ddb77a88c441a33392
spigkaht <benj@betterrentals.com.au>
2024-03-08 11:41:40 +1100

    Working my way through setting up Wepback for this project
    File structure setup, webpack config done.
    
    Currently creating HTML and CSS and then will
    need to work out how to template or similar so
    they will load together with JS

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    menu: './src/menu.js',
+    contact: './src/contact.js'
+  },  
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Odin Restaurant Page Project',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
